5d876c2d250d2d78d92a114d95c1d41570c8ed3b
prash_1 <prashantsharma_2k17ee157@dtu.ac.in>
2023-01-11 22:50:57 +0530

    promises

---
+++ React/Interview/Promises_Callback.js
@@ -0,0 +1,87 @@
+//callback async operation
+
+function firstFunction(cb){
+    setTimeout(()=>{
+        cb("given Callback")
+        console.log("First Promise is resolved")
+        
+    }, 1000)
+}
+function secondFunction(cb){
+    setTimeout(()=>{
+        cb("givenCallback")
+        console.log("Second Promise is resolved")
+    },1000)
+}
+
+
+const message = firstFunction((message)=>{
+    console.log(message)
+    secondFunction((mess)=>{
+        console.log(mess)
+    }
+    )
+
+})
+
+
+
+
+
+
+
+//Promise
+const first =  new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("First Promise resolved")
+    }, 1000)
+})
+
+const second =  new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Second Promise resolved")
+    }, 1000)
+})
+
+first.then((resolve)=>{
+    console.log(resolve)
+    second.then((res)=>{
+        console.log(res)
+    })
+}).catch(()=>{
+    console.log("Not resolved")
+})
+
+
+//promise chaining  calling another promise in return of promise (parent)
+
+const firstNew =  new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("First Promise resolved")
+    }, 1000)
+})
+
+const secondNew =  new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Second Promise resolved")
+    }, 1000)
+})
+
+firstNew.then((firstMessage)=>{
+    console.log(firstMessage)
+    return secondFunction
+})
+.then((secondMessage)=>{
+    return console.log(secondMessage)
+})
+
+
+
+//types of promises(Promise combinators)
+
+
+Promise.all([]).then().catch()   //run all the promises in parrallel , if any one fails it will return the Promise (it will Promise failed)
+Promise.allSettled([])  // run all the promises and return all the promised irrespective if the promise is failed or fullfilled
+
+Promise.race([]).the().catch()   // return the first promise irrespective of fullfilled or failed  (it will show state of first promised resolved)
+Promise.any([]).then().catch() //   returun all the
