3d94234d844af6d5877cd024e5fd418de2c3cc80
prash_1 <prashantsharma_2k17ee157@dtu.ac.in>
2023-01-12 19:57:17 +0530

    minor

---
+++ React/Interview/debounce.js
@@ -0,0 +1,19 @@
+// debounce 
+
+const optmiseDebounce = (func, delay) => {
+    let timer;
+    return function (){
+        let context = this, 
+        args= arguments
+        clearTimeout(timer);
+        timer =  setTimeout(()=>{
+                func.apply(context, args)
+        }, delay)
+    }
+}
+
+
+async function nameApi(){
+    
+}
+const debounce = optmiseDebounce(nameApi, 1000)
